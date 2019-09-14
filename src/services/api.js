8e7b271c6fcaf3cd989e8ad823da3e92a3c06b51
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-09-14 11:53:56 -0300

    Configurando Axios;

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,54 @@
 import axios from 'axios';
+import axios2 from 'axios';
+import { AsyncStorage } from 'react-native';
 
-/* Endereços para cada emulador/simulador:
-** Genymotion:              http://10.0.3.2:3333/
-** Emulador Android Studio: http://10.0.2.2:3333/
-** Simulador IOS:           http://localhost:3333/
-*/
-const api = axios.create({
-  // baseURL: 'http://10.0.3.2:3333/',
+import { rota } from '../env';
+
+axios.interceptors.request.use(config => {
+    config.baseURL = rota;
+
+    if (AsyncStorage.getItem('access')) {
+        config.headers.common['Authorization'] = `Bearer ${AsyncStorage.getItem('access')}`;
+    };
+
+    return config;
+}, error => {
+    return Promise.reject(error);
 });
 
-export default api;
+axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        const { config, response: { status } } = error;
+        const originalRequest = config;
+
+        if (status === 401 && config && !config.__isRetryRequest && AsyncStorage.getItem('refresh')) {
+            return axios.post('api/token-refresh/', { refresh: AsyncStorage.getItem('refresh') }).then(res => {
+                AsyncStorage.setItem('access', res.data.access);
+                originalRequest.headers['Authorization'] = `Bearer ${res.data.access}`;
+                return Promise.resolve(axios.request(originalRequest));
+            }, error => {
+                localStorage.clear();
+                return Promise.reject(error);
+            });
+        } else if (status === 401) {
+            localStorage.clear();
+            return Promise.reject(error);
+        };
+
+        return Promise.reject(error);
+    }
+);
+
+axios2.interceptors.request.use(config => {
+    config.baseURL = rota;
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+export { axios2 as apiNotToken };
+
+export default axios;
